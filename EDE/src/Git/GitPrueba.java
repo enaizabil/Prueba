01310e18b72d0d90dadda34b622ea3ef7a3f9524
Propietario <Propietario@pc-1557230263>
2020-05-12 10:27:42 +0200

    crear clae empleado

---
+++ EDE/src/Git/GitPrueba.java
@@ -0,0 +1,28 @@
+package Git;
+
+public class GitPrueba {
+	
+
+		private String nombre;
+		private String apellido;
+		private double salario; 
+
+
+	public GitPrueba(String nombre, String apellido, double salario) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.salario = salario;
+	}
+	public void subirsalario (double subida) {
+		salario=salario+subida;
+	}
+
+	private boolean comprobar() {
+		if (nombre.equals("")) {
+			return false;
+		}
+		return true;
+	}
+	}
+
+
